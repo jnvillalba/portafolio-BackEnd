b275c08e54b553d4fcfa4c37584345b6c47fe32e
jnvillalba <joaquinvillalba8@gmail.com>
2022-11-17 18:08:16 -0300

    cambios en Persona

--- src/main/java/com/porfolio/jnv/Controller/PersonaController.java
+++ src/main/java/com/porfolio/jnv/Controller/PersonaController.java
@@ -1,26 +1,104 @@
 package com.porfolio.jnv.Controller;
 
+import com.porfolio.jnv.Dto.dtoPersona;
 import com.porfolio.jnv.Entity.Persona;
-import com.porfolio.jnv.Interface.IPersonaService;
+import com.porfolio.jnv.Security.Controller.Mensaje;
+import com.porfolio.jnv.Service.ImpPersonaService;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/personas")
 @CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
-    @Autowired IPersonaService ipersonaservice;
+     @Autowired
+     ImpPersonaService sPersona;
+    
+    @GetMapping("/lista")
+    public ResponseEntity<List<Persona>> list(){
+        List<Persona> list = sPersona.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+    
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Persona> getById(@PathVariable("id")int id){
+        if(!sPersona.existsById(id)){
+            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.BAD_REQUEST);
+        }
+        
+        Persona persona = sPersona.getOne(id).get();
+        return new ResponseEntity(persona, HttpStatus.OK);
+    }
+    
+    /*@DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id){
+        if(!sPersona.existsById(id)){
+            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.NOT_FOUND);
+        }
+        sPersona.delete(id);
+        return new ResponseEntity(new Mensaje("Persona eliminada"), HttpStatus.OK);
+    }*/
+    
+    /*
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody dtoPersona dtopersona){
+        if(StringUtils.isBlank(dtopersona.getNombre())){
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+        if(sPersona.existsByNombre(dtopersona.getNombre())){
+            return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
+        }
+        
+        Persona persona = new Persona(
+                dtopersona.getNombre(), dtopersona.getDescripcion()
+            );
+        sPersona.save(persona);
+        return new ResponseEntity(new Mensaje("Educacion creada"), HttpStatus.OK);
+                
+    }
+    */
+    
+    
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoPersona dtopersona){
+        if(!sPersona.existsById(id)){
+            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.NOT_FOUND);
+        }
+        if(sPersona.existsByNombre(dtopersona.getNombre()) && sPersona.getByNombre(dtopersona.getNombre()).get().getId() != id){
+            return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
+        }
+        if(StringUtils.isBlank(dtopersona.getNombre())){
+            return new ResponseEntity(new Mensaje("El campo no puede estar vacio"), HttpStatus.BAD_REQUEST);
+        }
+        
+        Persona persona = sPersona.getOne(id).get();
+        
+        persona.setNombre(dtopersona.getNombre());
+        persona.setApellido(dtopersona.getApellido());
+        persona.setDescripcion(dtopersona.getDescripcion());
+        persona.setImg(dtopersona.getImg());
+        
+        sPersona.save(persona);
+        
+        return new ResponseEntity(new Mensaje("Persona actualizada"), HttpStatus.OK);
+    }
+    
+    
+}
+
+/*
+@Autowired IPersonaService ipersonaservice;
     
     @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
@@ -65,6 +143,4 @@ public class PersonaController {
         return persona;
         
     }
-    
-    
-}
+    */
--- src/main/java/com/porfolio/jnv/Service/ImpPersonaService.java
+++ src/main/java/com/porfolio/jnv/Service/ImpPersonaService.java
@@ -1,38 +1,46 @@
 package com.porfolio.jnv.Service;
 
+import com.porfolio.jnv.Entity.Experiencia;
 import com.porfolio.jnv.Entity.Persona;
-import com.porfolio.jnv.Interface.IPersonaService;
 import com.porfolio.jnv.Repository.IPersonaRepository;
 import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ImpPersonaService implements IPersonaService {
-    @Autowired IPersonaRepository ipersonarepository;
+@Transactional
+public class ImpPersonaService {
 
-    @Override
-    public List<Persona> getPersona() {
-        List<Persona> persona = ipersonarepository.findAll();
-        return persona;
+    @Autowired
+    IPersonaRepository ipersonarepository;
+
+    public List<Persona> list() {
+        return ipersonarepository.findAll();
     }
 
-    @Override
-    public void savePersona(Persona persona) {
+    public Optional<Persona> getOne(int id) {
+        return ipersonarepository.findById(id);
+    }
+
+    public Optional<Persona> getByNombre(String nombreE) {
+        return ipersonarepository.findByNombre(nombreE);
+    }
+
+    public void save(Persona persona) {
         ipersonarepository.save(persona);
     }
 
-    @Override
-    public void deletePersona(Long id) {
+    public void delete(int id) {
         ipersonarepository.deleteById(id);
     }
 
-    @Override
-    public Persona findPersona(Long id) {
-        Persona persona = ipersonarepository.findById(id).orElse(null);
-        return persona;
+    public boolean existsById(int id) {
+        return ipersonarepository.existsById(id);
     }
 
-    
-    
+    public boolean existsByNombre(String nombre) {
+        return ipersonarepository.existsByNombre(nombre);
+    }
 }
